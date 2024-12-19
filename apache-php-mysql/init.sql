21c9b1aec324dc60c75ff7a201c396dbf307dd92
CantonPo <cantoncorpas@gmail.com>
2024-12-19 12:02:42 +0100

    casi acabado

--- apache-php-mysql/init.sql
+++ apache-php-mysql/init.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE IF NOT EXISTS testdb;
 
+USE testdb;
+
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50),
-    password VARCHAR(50),
+    password VARCHAR(50)
 );
 
-
 INSERT INTO users (name, password) VALUES ('Dani', '1234'), ('Makram', '5678');
-
 
